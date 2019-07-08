c05b846989825986b4603a41373c6a407e594534
xiaoliang <834475460@qq.com>
2019-07-08 23:49:23 +0800

    第一次提交

---
+++ main/main.js
@@ -0,0 +1,26 @@
+var keys ={
+  0:['q','w','e','r','t','y','u','i','o','p'],
+  1:['a','s','d','f','g','h','j','k','l',],
+  2:['z','x','c','v','b','n','m'],
+  length: 3
+}
+var hash = {
+  q: 'qq.com',
+  w: 'weibo.com',
+  e: 'ele.me',
+  r: 'renren.com',
+  t: 'tianya.com',
+  y: 'yahoo.cn',
+  u: 'uc.com',
+  i: 'iqiyi.com',
+  o: 'opera.com',
+  p: undefined,
+  a: 'acfun.cn',
+  z: 'zhihu.com'
+}
+var p = document.createElement("p");
+document.body.appendChild(p);
+var div1 = document.createElement('div')
+document.body.appendChild(div1)
+var y=document.getElementById('x')
+y.appendChild(div1)
